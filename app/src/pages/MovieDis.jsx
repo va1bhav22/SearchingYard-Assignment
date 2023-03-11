2c2249c8ba0da4179e67506872f8295241fbc978
vaibhav21898 <vaibhav.jadhav@usdservices.in>
2023-03-11 23:26:58 +0530

    addreacticonscomplete

--- app/src/pages/MovieDis.jsx
+++ app/src/pages/MovieDis.jsx
@@ -4,8 +4,13 @@ import { useParams } from "react-router-dom";
 import { motion } from "framer-motion";
 import { fetchData } from "../Redux/action/action";
 import "../styles/DiscriptionPage.css";
+import { ImList2 } from "react-icons/im";
+import { AiFillHeart, AiFillStar } from "react-icons/ai";
+import { MdOutlineBookmarkAdd } from "react-icons/md";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import { FaPlay } from "react-icons/fa";
+
 const MovieDis = () => {
-  
   const { id } = useParams();
   console.log(id);
   const dispatch = useDispatch();
@@ -33,25 +38,79 @@ const MovieDis = () => {
           alt=""
         />
       </div>
-     <div className="CurruntPostMain">
-     <motion.div
-        initial={{ y: -370 }}
-        animate={{ y: 0 }}
-        transition={{ delay: 0.2, duration: 1 }}
-        className="DetailsContainerbg"
-      >
-        <div className="DiscripTionDetails1st">
-        <img className="img2ndpost" src={getPosterUrl(currentMovie?.poster_path)} alt="" />
-        </div>
-        <div className="DiscripTionDetails2st">
-          <div>{currentMovie?.title}</div>
-        </div>
-      </motion.div>
-     </div>
+      <div className="CurruntPostMain">
+        <motion.div
+          initial={{ y: -370 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 0.2, duration: 1 }}
+          className="DetailsContainerbg"
+        >
+          <div className="DiscripTionDetails1st">
+            <img
+              className="img2ndpost"
+              src={getPosterUrl(currentMovie?.poster_path)}
+              alt=""
+            />
+          </div>
+          <div className="DiscripTionDetails2st">
+            <div className="DiscriptionTitle">{currentMovie?.title}</div>
+            <div className="MovieDetails">
+              <div className="Ricon">R</div> &nbsp;
+              <div className="ReleseDate">{currentMovie?.release_date}</div>
+              &nbsp; <div>| {currentMovie?.original_language} |</div> &nbsp;
+              <div>Romance, Drama, Action</div> &nbsp; :<div> 1h 55m</div>
+            </div>
+            <div className="DiscriptionIcon">
+              <motion.div
+                whileHover={{ scale: 1.4 }}
+                transition={{ delay: 0.2, duration: 0.2 }}
+                className="FaPlay"
+              >
+                <ImList2 className="ReactIcons" />
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.4 }}
+                transition={{ delay: 0.2, duration: 0.2 }}
+                className="FaPlay"
+              >
+                <AiFillHeart className="ReactIcons" />
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.4 }}
+                transition={{ delay: 0.2, duration: 0.2 }}
+                className="FaPlay"
+              >
+                <MdOutlineBookmarkAdd className="ReactIcons" />
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.4 }}
+                transition={{ delay: 0.2, duration: 0.2 }}
+                className="FaPlay"
+              >
+                <BsFillPlayCircleFill className="ReactIcons" />
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.4 }}
+                transition={{ delay: 0.2, duration: 0.2 }}
+                className="FaPlay"
+              >
+                <AiFillStar className="ReactIcons" />
+              </motion.div>
+              <div className="FaPlaysingle">
+                <FaPlay /> &nbsp; Play Trailer
+              </div>
+            </div>
+            <div className="DisOverView">
+              <div className="Overview">Overview</div><br />
+             <div>
+             {currentMovie?.overview}
+             </div>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
 
 export default MovieDis;
-
-
