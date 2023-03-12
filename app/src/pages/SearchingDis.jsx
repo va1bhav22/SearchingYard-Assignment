7c7bf37deb806245f11981a08fc8daa3a63a7b60
vaibhav21898 <vaibhav.jadhav@usdservices.in>
2023-03-12 21:32:22 +0530

    serchsingle

--- app/src/pages/SearchingDis.jsx
+++ app/src/pages/SearchingDis.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { seachingData } from '../Redux/action/action';
+import { useSelector } from 'react-redux';
 const SearchingDis = () => {
     const { id } = useParams();
     console.log(id);
-    const dispatch = useDispatch();
-    const { search_data }= useSelector((store) => store.data);
+    
+    const { search_data }= useSelector((state) => state.data);
     console.log("search_data", search_data);
-    useEffect(() => {
-        dispatch(seachingData());
-      }, [dispatch]);
+   
   return (
     <div>SearchingDis</div>
   )
